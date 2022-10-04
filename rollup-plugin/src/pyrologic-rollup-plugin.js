6b649672e9a94f6a2b137c2e8dbdef2615542e33
pyrologic <64922965+pyrologic@users.noreply.github.com>
2022-10-04 21:17:41 +0200

    message filter corrected; "verboseOutput" added

--- rollup-plugin/src/pyrologic-rollup-plugin.js
+++ rollup-plugin/src/pyrologic-rollup-plugin.js
@@ -2,6 +2,8 @@ class PyrologicRollupPlugin {
 
     constructor() {
         this.wcount = 0;
+        this.fcount = 0;
+        this.verboseOutput = false;
         this.filterCDWarning = false;
     }
 
@@ -11,14 +13,16 @@ class PyrologicRollupPlugin {
 
     infoBuildStart() {
         this.wcount = 0;
+        this.fcount = 0;
     }
 
     infoBuildEnd() {
         const wc = this.wcount;
+        const ft = (this.verboseOutput && this.filterCDWarning) ? ` (${this.fcount} warnings filtered out.)` : '';
         if ( wc > 0 ) {
-            console.log('\x1b[33m%s\x1b[0m', `Build ended with ${wc} warnings!`);
+            console.log('\x1b[33m%s\x1b[0m', `Build ended with ${wc} warnings!${ft}`);
         } else {
-            console.log('\x1b[32m%s\x1b[0m', 'Build ended with no warnings.');
+            console.log('\x1b[32m%s\x1b[0m', `Build ended with no warnings.${ft}`);
         }
     }
 
@@ -39,6 +43,7 @@ class PyrologicRollupPlugin {
 
     infoPlugin(options = {}) {
         const opts = options || {};
+        this.verboseOutput = !!opts.verboseOutput;
         this.filterCDWarning = !!opts.filterCDWarning;
         const self = this;
         return {
@@ -64,10 +69,11 @@ class PyrologicRollupPlugin {
 
     onwarn ( { loc, frame, message } ) {
         if ( this.filterCDWarning ) {
-            if ( typeof message === 'string' && message.length > 0 ) {
+            if ( (typeof message === 'string') && (message.length > 0) ) {
                 const mup = message.toUpperCase();
-                if ( mup.includes('CIRCULAR') && message.includes('DEPENDENCY') ) {
+                if ( mup.includes('CIRCULAR') && mup.includes('DEPENDENCY') ) {
                     // ignore!
+                    ++this.fcount;
                     return;
                 }
             }
